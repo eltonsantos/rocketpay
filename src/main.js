7b425796bb626f96856109ecf2ffd3279db3ac78
Elton Santos <eltonaxl@hotmail.com>
2022-10-20 15:18:26 -0300

    Finish application

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import "./css/index.css";
-import imask from "imask";
+import IMask from "imask";
 
 const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path");
 const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path");
@@ -7,9 +7,16 @@ const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img");
 
 function setCardType(type) {
   const colors = {
+    americanexpress: ["black", "gray"],
     visa: ["#436D99", "#2D57F2"],
+    discover: ["black", "gray"],
+    diners: ["black", "gray"],
+    jcb15: ["black", "gray"],
+    jcb: ["black", "gray"],
+    maestro: ["black", "gray"],
+    unionpay: ["black", "gray"],
     mastercard: ["#DF6F29", "#C69347"],
-    rocketseat: ["#0D5F5D", "#C3129C"],
+    // rocketseat: ["#0D5F5D", "#C3129C"],
     default: ["black", "gray"],
   }
 
@@ -51,11 +58,46 @@ const cardNumberPattern = {
     {
       mask: "0000 0000 0000 0000",
       regex: /^4\d{0,15}/,
+      cardType: "americanexpress"
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /^3[47]\d{0,13}/,
       cardType: "visa"
     },
     {
       mask: "0000 0000 0000 0000",
-      regex: /(^5[1-5]\d{0,2}|^22[2-9]\d|^2[3-7]\d{0,2})\d{0-12}/,
+      regex: /^(?:6011|65\d{0,2}|64[4-9]\d?)\d{0,12}/,
+      cardType: "discover"
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /^3(?:0([0-5]|9)|[689]\d?)\d{0,11}/,
+      cardType: "diners"
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /^(?:2131|1800)\d{0,11}/,
+      cardType: "jcb15"
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /^(?:35\d{0,2})\d{0,12}/,
+      cardType: "jcb"
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /^(?:5[0678]\d{0,2}|6304|67\d{0,2})\d{0,12}/,
+      cardType: "maestro"
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /^62\d{0,14}/,
+      cardType: "unionpay"
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /(^5[1-5]\d{0,2}|^22[2-9]\d|^2[3-7]\d{0,2})\d{0,12}/,
       cardType: "mastercard"
     },
     {
@@ -73,3 +115,55 @@ const cardNumberPattern = {
   }
 }
 const cardNumberMasked = IMask(cardNumber, cardNumberPattern);
+
+const addButton = document.querySelector("#add-card");
+addButton.addEventListener("click", (e) => {
+  //e.preventDefault();
+  console.log("clicou");
+});
+
+document.querySelector("form").addEventListener("submit", (e) => {
+  e.preventDefault();
+})
+
+const cardHolder = document.querySelector("#card-holder");
+cardHolder.addEventListener("input", () => {
+  const ccHolder = document.querySelector(".cc-holder .value");
+  ccHolder.innerText = cardHolder.value.length === 0 ? "FULANO DA SILVA" : cardHolder.value;
+});
+
+securityCodeMasked.on("accept", () => {
+  updateSecurityCode(securityCodeMasked.value);
+});
+
+function updateSecurityCode(code) {
+  const ccSecurity = document.querySelector(".cc-security .value");
+  ccSecurity.innerText = code.length === 0 ? "123" : code;
+};
+
+// Por que não funciona?
+// securityCodeMasked.on("accept", function updateSecurityCode(code) {
+//   const ccSecurity = document.querySelector(".cc-security .value");
+//   ccSecurity.innerText = code.length === 0 ? "123" : code;
+// });
+
+
+cardNumberMasked.on("accept", () => {
+  const cardType = cardNumberMasked.masked.currentMask.cardType;
+  setCardType(cardType);
+  updateCardNumber(cardNumberMasked.value);
+});
+
+function updateCardNumber(number) {
+  const ccNumber = document.querySelector(".cc-number");
+  ccNumber.innerText = number.length === 0 ? "1234 5678 9012 3456" : number;
+}
+
+expirationDateMasked.on("accept", () => {
+  updateExpirationDate(expirationDate.value);
+})
+
+function updateExpirationDate(date) {
+  const ccExpiration = document.querySelector(".cc-extra .value");
+  ccExpiration.innerText = date.length === 0 ? "02/32" : date;
+}
